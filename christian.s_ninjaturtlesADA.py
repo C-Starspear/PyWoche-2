ea5644f78b3320fcf1ff234e848686b15e0a1eae
C-Starspear <christianspeernew@gmail.com>
2024-11-21 15:19:47 +0100

    Aufgabe 12: Hero Turtles

---
+++ christian.s_ninjaturtlesADA.py
@@ -0,0 +1,65 @@
+import turtle
+
+fenster = turtle.Screen()
+fenster.bgcolor("black")  
+
+leonardo = turtle.Turtle()
+leonardo.shape("turtle")
+leonardo.color("green")
+leonardo.speed(1)  
+
+def hilfslinie():
+    leonardo.penup()
+    leonardo.goto(-200, 0)  
+    leonardo.pendown()
+    leonardo.forward(390)  
+    leonardo.backward(390)
+
+def zeichne_a():
+    leonardo.pendown()
+    leonardo.left(60)
+    leonardo.forward(100)
+    leonardo.right(120)
+    leonardo.forward(100)
+    leonardo.backward(50)
+    leonardo.right(120)
+    leonardo.forward(50)
+    leonardo.penup()
+    leonardo.forward(20)  
+
+def zeichne_d():
+    leonardo.pendown()
+    leonardo.left(90)  
+    leonardo.backward(100)
+    leonardo.left(90)
+    leonardo.circle(-50, 180) 
+    leonardo.penup()
+    leonardo.forward(20) 
+
+def zeichne_a2():
+    leonardo.pendown()
+    leonardo.left(60)
+    leonardo.forward(100)
+    leonardo.right(120)
+    leonardo.forward(100)
+    leonardo.backward(50)
+    leonardo.right(120)
+    leonardo.forward(50)
+    leonardo.penup()
+    leonardo.forward(20)  
+
+leonardo.penup()
+leonardo.goto(-200, 0)  
+
+hilfslinie() 
+
+zeichne_a()  
+
+leonardo.goto(-30, 0) 
+zeichne_d() 
+
+leonardo.goto(90, 0)  
+leonardo.left(180)
+zeichne_a2() 
+
+turtle.done()
